eba419c0d2e09fc3401787030bb937b1987b2da8
Jamefrus <jamefrus@ya.ru>
2016-12-16 11:29:29 +0200

    Add network factory method

--- neuvi/src/main/kotlin/com/github/jnexil/neuvi/util/Layers.kt
+++ neuvi/src/main/kotlin/com/github/jnexil/neuvi/util/Layers.kt
@@ -1,6 +1,10 @@
 package com.github.jnexil.neuvi.util
 
 import com.github.jnexil.neuvi.api.Activation.*
+import com.github.jnexil.neuvi.api.Direction.*
+import com.github.jnexil.neuvi.api.layers.*
+import com.github.jnexil.neuvi.api.networks.*
+import com.github.jnexil.neuvi.api.providers.*
 import com.github.jnexil.neuvi.api.providers.LayerProvider.Companion.layer
 
 object Layers {
@@ -9,4 +13,15 @@ object Layers {
 
     fun linear() = layer(1, Linear)
     fun sigmoid() = layer(1, Linear)
+
+    fun network(vararg layers: FlexibleLayer): Network {
+        require(layers.size < 2) { "Network should contain not less than 2 layes" }
+        val input = layers.first()
+        val output = layers.last()
+        layers.reduce { first, second ->
+            first.attach(RIGHT, second)
+            second
+        }
+        return Network.FromMutable[MemoryProvider, input, output]
+    }
 }
